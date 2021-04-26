6d13aa8f987c9f8d4878e2b318a916b1793f82f4
fazioandrea.f <fazioandrea.f@gmail.com>
2021-04-26 16:30:09 +0200

    inserito bk image sul jumbo

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let mix = require('laravel-mix');
 
 mix
 .copy('src/index.html','dist')
-.sass('src/style.scss','')
+.sass('src/style.scss','').options({
+    processCssUrls: false})
 .js('src/app.js', '')
 .setPublicPath('dist');
